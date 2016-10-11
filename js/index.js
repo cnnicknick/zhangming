06c98b08e3e29ea87c7f6fceffddf71085a3be66
ZhangMing <980234483@qq.com>
2016-10-11 22:18:35 +0800

    增加动画效果

--- js/index.js
+++ js/index.js
@@ -1,10 +1,6 @@
-/*!
- * Start Bootstrap - Agency Bootstrap Theme (http://startbootstrap.com)
- * Code licensed under the Apache License v2.0.
- * For details, see http://www.apache.org/licenses/LICENSE-2.0.
- */
 
-// jQuery for page scrolling feature - requires jQuery Easing plugin
+
+// 点击页面导航条时控制页面滚动方式，需要加载jQuery Easing插件
 $(function() {
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
@@ -15,12 +11,12 @@ $(function() {
     });
 });
 
-// Highlight the top nav as scrolling occurs
+// 页面滚动时高亮顶部目标导航条
 $('body').scrollspy({
     target: '.navbar-fixed-top'
 })
 
-// Closes the Responsive Menu on Menu Item Click
+// 响应菜单单击时后自动关闭菜单项
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
